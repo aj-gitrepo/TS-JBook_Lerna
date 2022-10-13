04f172dd7c67f663675c795eb1d9b6103da23e11
wink-aajlak <aajlak.wink@gmail.com>
2022-10-13 11:55:00 +0530

    development port scenario

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,8 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
+    // for development using app in port 3000, now if 4005 is opend the app opens
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+    }));
     // to enable the try catch block (in cli/serve.ts) to work
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
+
+  // for development using app in port 3000, now if 4005 is opend the app opens
+  app.use(
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent',
+    })
+  );
+
   // to enable the try catch block (in cli/serve.ts) to work
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
